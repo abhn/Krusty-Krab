88914073ef5ae67a6a630a89b12e032e2ba06c4d
Abhishek Nagekar <abhishek@nagekar.com>
2023-02-18 00:37:16 +0100

    Type casted entered number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,13 +7,16 @@ fn main() {
     println!("Please enter the number.");
 
     let mut guess = String::new();
-    let random_number = rand::thread_rng().gen_range(1..=100);
 
     io::stdin()
       .read_line(&mut guess)
       .expect("Failed");
 
     println!("You guessed: {guess}");
+
+    let guess: i32 = guess.trim().parse().expect("Enter a valid number");
+
+    let random_number = rand::thread_rng().gen_range(1..=100);
 
     match guess.cmp(&random_number) {
       Ordering::Less => println!("Too low"),
